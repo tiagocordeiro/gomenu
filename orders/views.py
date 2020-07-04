0f9e2c495c9f030a5420333dd7178cf9ec534ef9
Tiago Cordeiro <tiago@mulhergorila.com>
2020-07-04 18:17:13 -0300

    Agora é possível adicionar produtos variáveis ao carrinho
    relacionado a issue #18

--- orders/views.py
+++ orders/views.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 from menus.models import Menu
 from orders.forms import OrderItemsForm, OrderForm
 from orders.models import Order, OrderItem
-from products.models import Product
+from products.models import Product, Variation
 from restaurants.models import Restaurant
 
 
@@ -43,7 +43,51 @@ def order_add_item(request, pk, restaurant_pk, menu_pk, **kwargs):
         item = OrderItem(order=order, item=product, unity_price=product.price)
         item.save()
 
-    messages.success(request, f"{item.item.name} adicionado ao pedido")
+    messages.success(request, f'{item.item.name} adicionado ao pedido. '
+                              f'<a href="/orders/cart/{order_slug}/" class="alert-link">Ver pedido</a>.')
+    return redirect(reverse('menu_display', kwargs={'pk': menu.pk,
+                                                    'slug': menu.slug}))
+
+
+def order_add_var_item(request, pk, var_pk, restaurant_pk, menu_pk, **kwargs):
+    product = Product.objects.get(pk=pk)
+    variation = Variation.objects.get(pk=var_pk)
+    menu = Menu.objects.get(pk=menu_pk)
+    restaurant = Restaurant.objects.get(pk=restaurant_pk)
+    if request.user.is_authenticated:
+        pass
+        # TODO: Lógica para usuários autenticados
+
+    try:
+        order_slug = request.session["order_slug"]
+    except KeyError:
+        order = Order(restaurant=restaurant)
+
+        order.save()
+        request.session["order_slug"] = order_slug = order.slug
+
+    order = Order.objects.get(slug=order_slug)
+    if order.orderitem_set.all().filter(item=product,
+                                        variation=variation).exists():
+
+        item = OrderItem.objects.get(order=order, item=product)
+        item.quantity = item.quantity + 1
+        item.save()
+        messages.info(request,
+                      f'{product.name} Já está no pedido. '
+                      f'<a href="/orders/cart/{order_slug}/" class="alert-link">Ver pedido</a>.')
+
+        return redirect(reverse('menu_display', kwargs={'pk': menu.pk,
+                                                        'slug': menu.slug}))
+
+    else:
+        item = OrderItem(order=order, item=product,
+                         unity_price=product.productvariation_set.get(
+                             variation=variation).price, variation=variation)
+        item.save()
+
+    messages.success(request, f'{item.item.name} adicionado ao pedido. '
+                              f'<a href="/orders/cart/{order_slug}/" class="alert-link">Ver pedido</a>.')
     return redirect(reverse('menu_display', kwargs={'pk': menu.pk,
                                                     'slug': menu.slug}))
 
