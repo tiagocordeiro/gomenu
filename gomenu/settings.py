a7c9474db88752e45bb9f3972c28bedb8945becf
Tiago Cordeiro <tiago@mulhergorila.com>
2020-07-31 15:00:02 -0300

    SetUp DjangoDebugToolbar
    This close #51

--- gomenu/settings.py
+++ gomenu/settings.py
@@ -59,6 +59,12 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+# Django DebugToolbar
+if DEBUG:
+    INSTALLED_APPS.append('debug_toolbar')
+    MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
+    INTERNAL_IPS = ['127.0.0.1']
+
 ROOT_URLCONF = "gomenu.urls"
 
 TEMPLATES = [
--- gomenu/urls.py
+++ gomenu/urls.py
@@ -28,3 +28,9 @@ urlpatterns = [
     path('menus/', include('menus.urls')),
     path('orders/', include('orders.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__', include(debug_toolbar.urls)),
+    ] + urlpatterns
