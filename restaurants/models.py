efa057eaee2d77f9e7894fc20be473f683823fad
Tiago Cordeiro <tiago@mulhergorila.com>
2020-11-01 10:03:54 -0300

    Restaurant friendly url
    Adiciona urls amigáveis para o restaurante e seus menus.
    
    Relacionado a Issue #106
    this close #89

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils.text import slugify
 
 
 class Restaurant(models.Model):
@@ -9,6 +10,21 @@ class Restaurant(models.Model):
     image = models.ImageField(upload_to='restautant/', blank=True, null=True)
     manager = models.OneToOneField(User, null=True, on_delete=models.SET_NULL)
     online_sales = models.BooleanField("Vendas online", default=False)
+    slug = models.SlugField(max_length=255, unique=True, verbose_name="slug",
+                            help_text="Preenchido automaticamente, não editar.",
+                            null=True,
+                            blank=True, )
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'restaurante'
+        verbose_name_plural = 'restaurantes'
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.name)
+
+        super(Restaurant, self).save(*args, **kwargs)
 
     def __str__(self):
         return str(self.name)
--- restaurants/urls.py
+++ restaurants/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.my_restaurant, name='my_restaurant'),
-    path('new/', views.new_restaurant, name='new_restaurant'),
-    path('detail/<pk>/', views.restaurant_detail, name='restaurant_detail'),
+    path('restaurant/', views.my_restaurant, name='my_restaurant'),
+    path('restaurant/new/', views.new_restaurant, name='new_restaurant'),
+    path('restaurant/detail/<pk>/', views.restaurant_detail, name='restaurant_detail'),
+    path('<slug>/', views.restaurant_main, name='restaurant_main'),
+    path('<restaurant_slug>/<menu_slug>/', views.restaurant_menu, name='restaurant_menu'),
 ]
