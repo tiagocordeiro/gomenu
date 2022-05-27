7e8440fa7ec518218acdd8bd970d1cce42665a73
Tiago Cordeiro <tiago@mulhergorila.com>
2022-05-27 14:57:14 -0300

    Improve products list in Django admin
    This close #300

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,6 @@
-from django_summernote.admin import SummernoteModelAdmin
 from admin_ordering.admin import OrderableAdmin
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 
 from .models import Product, Category, ProductVariation
 
@@ -16,29 +16,40 @@ class CategoryAdmin(SummernoteModelAdmin):
     list_filter = ('restaurant',)
 
 
-class VariationAdmin(admin.ModelAdmin):
-    list_display = ('name', 'restaurant')
-    list_filter = ('restaurant',)
+class ProductVariationAdmin(admin.ModelAdmin):
+    search_fields = ["product"]
+    list_display = ('product', 'variation', 'price')
+    list_filter = ('product__restaurant',)
+    list_editable = ['variation', 'price']
 
 
 class ProductAdmin(OrderableAdmin, admin.ModelAdmin):
     # The field used for ordering. Prepend a minus for reverse
     # ordering: "-order"
     ordering_field = "order"
+    search_fields = ["name"]
 
     # You may optionally hide the ordering field in the changelist:
     # ordering_field_hide_input = False
 
     # The ordering field must be included both in list_display and
     # list_editable:
-    list_display = ('name', 'order', 'category', 'restaurant')
-    list_editable = ["order"]
+    list_display = ('name', 'price', 'product_type', 'order', 'category', 'restaurant')
+    list_editable = ["order", "price"]
 
     list_filter = ('category', 'restaurant')
     inlines = [
         ProductVariationInLine,
     ]
 
+    @staticmethod
+    def product_type(obj):
+        if len(obj.productvariation_set.all()) >= 1:
+            return 'variavel'
+        else:
+            return 'simples'
+
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(ProductVariation, ProductVariationAdmin)
